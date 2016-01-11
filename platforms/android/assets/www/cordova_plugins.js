c7667c4d3efac4e867a3ae17c4abbc5da38d79c6
accomplix <Yan@Yans-iMac.local>
2016-01-11 16:09:26 -0500

    Reorg.

--- platforms/android/assets/www/cordova_plugins.js
+++
@@ -1,9 +0,0 @@
-cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
-module.exports.metadata = 
-// TOP OF METADATA
-{
-    "cc.fovea.plugins.inapppurchase": "3.4.1"
-}
-// BOTTOM OF METADATA
-});
